803980ab41d1dfdb0f287e6c47534074d684e3ba
lasithdulshan369-commits <lasithdulshan369@gmail.com>
2025-08-31 19:13:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " sjsnssjshhshrhrbbb", // ඔයාගෙ session id එක
-MONGODB: process.env.MONGODB || "mongodb://mongo:sjjkkd",  //ඔයාගෙ mongoDb url එක
+SESSION_ID: process.env.SESSION_ID || "iJ8QjISQ#jAtnrEAddY8S0oGth1U53LsLbdk7nCVY8xU9r8tklIA", // ඔයාගෙ session id එක
+MONGODB: process.env.MONGODB || "postgresql://postgres:MGVwWesrHTECchIXVLgVYKXgjZrSlbCX@interchange.proxy.rlwy.net:52880/railway",  //ඔයාගෙ mongoDb url එක
 };
  
